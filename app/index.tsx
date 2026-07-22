82766ccaf476b5723094408a763f7d03c0fb56d7
Meshack T koontse <meshackkoontse9@gmail.com>
2026-07-22 21:12:26 +0200

    added splash screen on the index

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,7 @@
 import { Redirect } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync().catch(() => {});
 
 export default function Index() {
   return <Redirect href="/(tabs)" />;
