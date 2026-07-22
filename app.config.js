49e2db8f232c6898123cd239d3b76962817f6b38
Meshack T koontse <meshackkoontse9@gmail.com>
2026-07-22 14:34:01 +0200

    logo path

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
     slug: "recurrly",
     version: "1.0.0",
     orientation: "portrait",
-    icon: "./assets/images/icon.png",
+    icon: "./assets/images/logo.png",
     scheme: "recurrly",
     userInterfaceStyle: "automatic",
     newArchEnabled: true,
